afddee8853ef0cc3efd4fb81d5b9d3ec7fef98d0
sachin jain <sachinjain504.com>
2023-02-23 14:59:07 +0530

    uncomittedd

---
+++ src/Line/Line.h
@@ -0,0 +1,7 @@
+#ifndef _LINE_H_
+#define _LINE_H_
+#include <iostream>
+#include <>
+class Line 
+
+#endif 
--- src/Vertex/Vertex.cpp
+++ src/Vertex/Vertex.cpp
@@ -55,11 +55,11 @@ void Vertex::translate (glm::vec3 movementVec3 ){
     glm::translate ( verticeMatrix  , movementVec3 )  ; 
 } ;  
 void Vertex::rotateVertex ( const glm::vec3 & orientationVec3) {
-    vertex = glm::rotate (verticeMatrix,
+    verticeMatrix= glm::rotate (verticeMatrix,
     orientationVec3.x ,glm::vec3(1.0f , 0.0f , 0.0f ) );
-    vertex = glm::rotate (verticeMatrix ,
+    verticeMatrix = glm::rotate (verticeMatrix ,
     orientationVec3.y ,glm::vec3 (0.0f , 1.0f , 0.0f ) );
-    vertex = glm::rotate (verticeMatrix ,
+    verticeMatrix = glm::rotate (verticeMatrix ,
     orientationVec3.z ,glm::vec3 ( 0.0f , 0.0f , 1.0f));  
     return ;
 }  
